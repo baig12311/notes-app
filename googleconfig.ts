420679fcdb6ec53ed6dbf160cb7bdcc654d7e3ad
baig12311 <shaheerbaig664@gmail.com>
2026-05-21 05:26:22 +0500

    Remove google config file

--- googleconfig.ts
+++
@@ -1,6 +0,0 @@
-import { GoogleSignin } from '@react-native-google-signin/google-signin';
-GoogleSignin.configure({
-  webClientId: 
-  '481814952681-bnimtuq67ucfnvb7hngnng9ehnpsmk7s.apps.googleusercontent.com',
-});
-export default GoogleSignin;
